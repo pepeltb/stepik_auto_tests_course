2dbaa505f97ca3624c0b14a98657aa693855dfd8
Alexander Kutsenko <blackboss324@gmail.com>
2022-12-21 09:23:58 +0500

    Add files to homework

---
+++ test_items.py
@@ -0,0 +1,15 @@
+from selenium.webdriver.common.by import By
+import pytest
+#import time
+
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/.'
+
+def test_existence_of_add_button(browser):
+    browser.get(link)
+    #time.sleep(20) #для наглядной проверки убрать "#" с импорта и команды
+    browser.implicitly_wait(15)
+    add_to_shopping_сart_button = browser.find_element(By.CLASS_NAME, "btn.btn-lg.btn-primary.btn-add-to-basket") #ищем кнопку
+    assert add_to_shopping_сart_button, 'Add to cart button does not exist' #проверяем наличие кнопки
+    
+
+
